b5ce0fa7017ba6edff90a278f8418403aaeb105f
Ufuk Gömeçoğlu <108555386+ufukgomecoglu@users.noreply.github.com>
2022-11-06 15:32:24 +0300

    .

--- DepoGirisApp/KullaniciGiris.Designer.cs
+++ DepoGirisApp/KullaniciGiris.Designer.cs
@@ -34,11 +34,15 @@
             this.tb_kullaniciAdi = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.btn_giris);
             this.groupBox1.Controls.Add(this.tb_sifre);
             this.groupBox1.Controls.Add(this.tb_kullaniciAdi);
@@ -46,7 +50,7 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(328, 133);
+            this.groupBox1.Size = new System.Drawing.Size(328, 148);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Kullanıcı Bilgileri ile Giriş";
@@ -96,11 +100,33 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Kullanıcı Adı:";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(18, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ufuk GÖMEÇOĞLU";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(140, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Hasan Hüseyin BADAK";
+            // 
             // KullaniciGiris
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(352, 154);
+            this.ClientSize = new System.Drawing.Size(352, 169);
             this.Controls.Add(this.groupBox1);
             this.Name = "KullaniciGiris";
             this.Text = "Kullanıcı Giriş ";
@@ -119,5 +145,7 @@
         private System.Windows.Forms.TextBox tb_kullaniciAdi;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
